27435c2e6a5297a55238633edc2c79b5807a1e5f
shivareddy17 <shivareddych17@gmail.com>
2025-03-13 23:08:05 +0530

    added expection handling for some end points

--- prisma1/src/index.ts
+++ prisma1/src/index.ts
@@ -1,172 +1,190 @@
 import { PrismaClient } from "@prisma/client";
-import express from 'express'
-import jwt from 'jsonwebtoken'
-import cors from 'cors'
+import express from "express";
+import jwt from "jsonwebtoken";
+import cors from "cors";
 
 import { userAuth } from "./middleware";
-const Jwt_Screet="chekkaNinja"
-const client =new PrismaClient();
-const app=express()
-app.use(express.json())
-app.use(cors())
-app.post("/signup",async(req,res)=>{
-    const {userName,password,firstName,lastName}=req.body
+const Jwt_Screet = "chekkaNinja";
+const client = new PrismaClient();
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.post("/signup", async (req, res) => {
+  const { userName, password, firstName, lastName } = req.body;
+  try {
     const response = await client.user.create({
-        data: {
-            userName,
-            password,
-            firstName,
-            lastName
-        }
+      data: {
+        userName,
+        password,
+        firstName,
+        lastName,
+      },
     });
     res.json({
-        msg:"user created"
-    })
-})
-    
-app.post("/signin",async(req,res)=>{
-    const {userName,password} =req.body
-const response= await client.user.findFirst({
-    where:{
-        userName:userName
-    }
-})
-const token=jwt.sign({id:response?.id},Jwt_Screet)
-if(response?.password===password){
-res.json({token:token})
-}
-console.log('signin called',userName,password)
-
-})
-app.get('/users/:id',userAuth,async(req,res)=>{
-    const id=req.params.id
-    const response=await client.user.findMany({
-        where:{
-            id:parseInt(id)
-        }
-        
-    })
-    res.json(response)
-})
-
-app.post('/todos',userAuth,async(req,res)=>{
-    const {title,description}=req.body
-    const id=req.userId
-    
-    if (id !== undefined) {
-        try{
-        const response = await client.todo.create({
-            data: {
-                title,
-                description,
-                done: false,
-                user_id: parseInt(id)
-            }
-        });
-        res.json({msg:"todo created"});}
-        catch(e){
-            console.log(e)
-        }
-    } else {
-        res.json({ msg: "id is null" });
-    }
-})
-
-app.get("/todos",userAuth,async(req,res)=>{
-const id=req.userId
-if(id!==undefined){
-    const response= await client.todo.findMany({
-        where:{
-            user_id:parseInt(id)
-        }
-    })
+      msg: "user created",
+    });
+  } catch (e) {
     res.json({
-        response
+      msg: "problem in creating user try again",
+    });
+  }
+});
+
+app.post("/signin", async (req, res) => {
+  const { userName, password } = req.body;
+  try{
+  const response = await client.user.findFirst({
+    where: {
+      userName: userName,
+    },
+  });
+  const token = jwt.sign({ id: response?.id }, Jwt_Screet);
+  if (response?.password === password) {
+    res.json({ token: token });
+  }else{
+    res.json({
+        msg:"invalid credentials"
     })
-}else{
-res.json({
-    msg:"user_id is null"
-})
-    
+  }
+}catch(e){
+    res.json({
+        msg:"some error occured, try again"
+    })
 }
-})
+});
+app.get("/users/:id", userAuth, async (req, res) => {
+  const id = req.params.id;
+  try{
+  const response = await client.user.findMany({
+    where: {
+      id: parseInt(id),
+    },
+  });
+  res.json(response);
+}catch(e){
+    res.json({
+        msg:"problem in finding user try aagin"
+    })
+}
+});
 
-app.put("/todos/:id",userAuth,async(req,res)=>{
-    const userId=req.userId
-    const id=req.params.id
-    const{title,description,done}=req.body
-    if(userId !==undefined){
+app.post("/todos", userAuth, async (req, res) => {
+  const { title, description } = req.body;
+  const id = req.userId;
 
-        const response= await client.todo.update({
-            where:{
-               user_id:parseInt(userId),
-               id:parseInt(id)
-            },
-            data:{
-                title:title,
-                description:description,
-                done:done
-            }
-        })
+  if (id !== undefined) {
+    try {
+      const response = await client.todo.create({
+        data: {
+          title,
+          description,
+          done: false,
+          user_id: parseInt(id),
+        },
+      });
+      res.json({ msg: "todo created" });
+    } catch (e) {
+      console.log(e);
+    }
+  } else {
+    res.json({ msg: "id is null" });
+  }
+});
+
+app.get("/todos", userAuth, async (req, res) => {
+  const id = req.userId;
+  if (id !== undefined) {
+    try{
+    const response = await client.todo.findMany({
+      where: {
+        user_id: parseInt(id),
+      },
+    });
+    res.json({
+      response,
+    });}catch(e){
         res.json({
-            msg:"todo updated"
-        })
-        console.log("edit called")
-    }else{
-        res.json({
-            msg:"id is undefined"
+            msg:"problem in finding todos"
         })
     }
-})
+  } else {
+    res.json({
+      msg: "user_id is null",
+    });
+  }
+});
 
-app.put("/done/:id",userAuth,async(req,res)=>{
-    const userId=req.userId
-    const id=req.params.id
-    const done=req.body.done
-    if(userId !==undefined){
+app.put("/todos/:id", userAuth, async (req, res) => {
+  const userId = req.userId;
+  const id = req.params.id;
+  const { title, description, done } = req.body;
+  if (userId !== undefined) {
+    const response = await client.todo.update({
+      where: {
+        user_id: parseInt(userId),
+        id: parseInt(id),
+      },
+      data: {
+        title: title,
+        description: description,
+        done: done,
+      },
+    });
+    res.json({
+      msg: "todo updated",
+    });
+    console.log("edit called");
+  } else {
+    res.json({
+      msg: "id is undefined",
+    });
+  }
+});
 
-        const response= await client.todo.update({
-            where:{
-               user_id:parseInt(userId),
-               id:parseInt(id)
-            },
-            data:{
-                
-                done:done
-            }
-        })
-        console.log("done called"+id,userId,done)
-        res.json({
-            msg:"todo done"
-        })
-    }else{
-        res.json({
-            msg:"id is undefined"
-        })
-    }
-})
+app.put("/done/:id", userAuth, async (req, res) => {
+  const userId = req.userId;
+  const id = req.params.id;
+  const done = req.body.done;
+  if (userId !== undefined) {
+    const response = await client.todo.update({
+      where: {
+        user_id: parseInt(userId),
+        id: parseInt(id),
+      },
+      data: {
+        done: done,
+      },
+    });
+    console.log("done called" + id, userId, done);
+    res.json({
+      msg: "todo done",
+    });
+  } else {
+    res.json({
+      msg: "id is undefined",
+    });
+  }
+});
 
+app.delete("/delete/:id", userAuth, async (req, res) => {
+  const userId = req.userId;
+  const id = req.params.id;
+  if (userId !== undefined) {
+    const response = await client.todo.delete({
+      where: {
+        user_id: parseInt(userId),
+        id: parseInt(id),
+      },
+    });
+    res.json({
+      msg: "todo deleted",
+    });
+    console.log(id);
+  } else {
+    res.json({
+      msg: "id is undefined",
+    });
+  }
+});
 
-app.delete("/delete/:id",userAuth,async(req,res)=>{
-    const userId=req.userId
-    const id=req.params.id
-    if(userId !==undefined){
-
-        const response= await client.todo.delete({
-            where:{
-               user_id:parseInt(userId),
-               id:parseInt(id)
-            }
-        })
-        res.json({
-            msg:"todo deleted"
-        })
-        console.log(id)
-    }else{
-        res.json({
-            msg:"id is undefined"
-        })
-    }
-})
-
-app.listen(3000)
+app.listen(3000);
